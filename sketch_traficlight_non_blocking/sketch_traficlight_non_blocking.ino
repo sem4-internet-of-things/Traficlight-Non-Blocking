d110b19e0b2c4515ad7057e06daea1cc88f703c7
Gonron <lundsgaard1997@hotmail.dk>
2019-05-29 10:41:04 +0200

    -

--- sketch_traficlight_non_blocking/sketch_traficlight_non_blocking.ino
+++ sketch_traficlight_non_blocking/sketch_traficlight_non_blocking.ino
@@ -1,39 +1,26 @@
 // set pin numbers:
-
 const int redPin =  13;
-
 const int yellowPin =  12;
-
 const int greenPin =  11;
 
 // Variables will change:
-
 int redState = LOW;
-
 int yellowState = LOW;
-
 int greenState = LOW;
-
 long StartTime = 0;
-
 long interval = 30000;
 
 void setup() {
 
   pinMode(redPin, OUTPUT);
-
   pinMode(yellowPin, OUTPUT);
-
   pinMode(greenPin, OUTPUT);
 
 }
 
-void loop()
-
-{
+void loop() {
 
   unsigned long CheckTime = millis();
-
   unsigned long elapsedTime = CheckTime - StartTime;
 
   if (elapsedTime > interval) {
@@ -43,39 +30,37 @@ void loop()
   }
 
   if (elapsedTime < 10000) {
-
-    redState = LOW;
-
-    yellowState = LOW;
-
-    greenState = HIGH;
-
+    green();
   }
 
   if (elapsedTime > 10000 && elapsedTime < 15000 ) {
-
-    redState = LOW;
-
-    yellowState = HIGH;
-
-    greenState = LOW ;
-
+    yellow();
   }
 
   if (elapsedTime > 15000  ) {
-
-    redState = HIGH;
-
-    yellowState = LOW;
-
-    greenState = LOW ;
-
+    red();
   }
 
   digitalWrite(redPin, redState);
-
   digitalWrite(greenPin, greenState);
-
   digitalWrite(yellowPin, yellowState);
 
+}
+
+void green() {
+  redState = LOW;
+  yellowState = LOW;
+  greenState = HIGH;
+}
+
+void yellow() {
+  redState = LOW;
+  yellowState = HIGH;
+  greenState = LOW ;
+}
+
+void red() {
+  redState = HIGH;
+  yellowState = LOW;
+  greenState = LOW ;
 }
